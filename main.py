33237d1a3f221cb46a780894fc6b52dfc07d01f5
Undress <andrejs.proskurins@gmail.com>
2018-12-07 01:14:12 +0200

    Доработал до конца

--- main.py
+++ main.py
@@ -1,15 +1,67 @@
-import  json
+import json
+import xml.etree.ElementTree as ET
+
+
+
 
 def main():
-    with open("newsafr.json") as file:
-        x = json.load(file)
 
-    y = []
+    words_json = parse_json('newsafr.json')
+    words_xml = parse_xml('newsafr.xml')
 
-    for news in x["rss"]["channel"]["items"]:
-        y.append(news["description"])
+    print(search_top(words_json))
+    print(search_top(words_xml))
 
-    print(y)
+
+def parse_xml(xml_file):
+
+    words = []
+
+    news = ET.parse(xml_file)
+
+    articles = news.findall('channel/item/description')
+
+    for article in articles:
+        for word in article.text.split(" "):
+            words.append(word)
+
+    return words
+
+
+def parse_json(json_file):
+
+    words = []
+
+    with open(json_file) as file:
+        news = json.load(file)
+
+    for article in news["rss"]["channel"]["items"]:
+        for word in article["description"].split(" "):
+            words.append(word)
+
+    return words
+
+
+
+def search_top(words):
+
+    final_words = []
+    final_dict = {}
+
+    for word in words:
+        if len(word) > 6:
+            final_words.append(word)
+
+    for word in final_words:
+        if not(word in final_dict):
+            final_dict.update({word : 1})
+        else:
+            final_dict[word] += 1
+
+    result = sorted(final_dict.items(), key=lambda x:x[1], reverse=True)[:10]
+
+    return result
+
 
 
 if __name__ == '__main__':
